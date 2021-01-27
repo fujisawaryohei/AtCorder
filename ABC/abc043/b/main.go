26313874859a3716f4306788f4f4770fe6452dca
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2021-01-27 10:01:19 +0900

    add abc43 b

--- ABC/abc043/b/main.go
+++ ABC/abc043/b/main.go
@@ -11,11 +11,20 @@ func main() {
 	stdin := bufio.NewScanner(os.Stdin)
 	stdin.Scan()
 	s := stdin.Text()
-	input := strings.Split(s, " ")
-	var ans *[]string
-	for _, str := range input {
-		//https://medium.com/veltra-engineering/is-the-golang-slice-a-pointer-or-not-a-pointer-99433f2cea17
-		*ans = append(*ans, str)
+	input := strings.Split(s, "")
+	var ansStr string
+	var ans []string
+	for i := 0; i < len(input); i++ {
+		if input[i] == "B" {
+			if len(ans) > 0 {
+				ans = ans[:len(ans)-1]
+			}
+		} else {
+			ans = append(ans, input[i])
+		}
 	}
-	fmt.Println(ans)
+	for _, str := range ans {
+		ansStr += str
+	}
+	fmt.Println(ansStr)
 }
