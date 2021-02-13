22e12b52f52cdf354d5e61c7d5ad320215805cae
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2021-02-13 16:26:47 +0900

    abc044 b golang 追加

--- ABC/abc044/b/main.go
+++ ABC/abc044/b/main.go
@@ -1,12 +1,54 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	arr := []string{"a", "b", "a", "c"}
-	for i := 0; i < len(arr)-1; i++ {
-		for k := 1; k < len(arr)-i; k++ {
-			if arr[k] > arr[k+1] {
-				arr[k], arr[k+1] = arr[k+1], arr[k]
-			}
+	var a string
+	fmt.Scan(&a)
+	w := strings.Split(a, "")
+	m := make(map[string]bool)
+	var uniq []string
+	var ansArr []int
+	var ans []int
+
+	for _, el := range w {
+		_, ok := m[el]
+		if !ok {
+			m[el] = true
+			uniq = append(uniq, el)
 		}
 	}
+
+	for _, el1 := range uniq {
+		count := 0
+		for _, el2 := range w {
+			if el1 == el2 {
+				count++
+			}
+		}
+		ansArr = append(ansArr, count)
+	}
+
+	for _, el := range ansArr {
+		if el%2 == 0 {
+			ans = append(ans, el)
+		}
+	}
+
+	if len(ans) == len(uniq) {
+		fmt.Println("Yes")
+	}
+	if len(ans) != len(uniq) {
+		fmt.Println("No")
+	}
+}
+
+func remove(arr []string, i int) []string {
+	slice1 := arr[:i]
+	slice2 := arr[i+1:]
+	newArr := append(slice1, slice2...)
+	return newArr
 }
